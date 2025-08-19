41d865cfe61633b8350e1504ff22773ae52cc298
guy542 <colly.guy@wanadoo.fr>
2025-08-20 01:21:15 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
 
+const config: CapacitorConfig = {
+  appId: 'fr.maires.app',
+  appName: 'Maires',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
